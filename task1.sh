d238ced399aa29be1a881267446c3d7b072b260c
alviitayyaba <95637336+alviitayyaba@users.noreply.github.com>
2022-04-20 16:12:27 +0500

    shell script
    .

--- task1.sh
+++ task1.sh
@@ -4,3 +4,4 @@ samtools view -h miniMNM00065.bam 1 > miniMNM00065.ch1.bam  ##Extract reads alig
 samtools fasta miniMNM00065.ch1.bam -F 4 > miniMNM00065.ch1.fasta ##Convert aligned reads into fasta reads
 bwa index Homo_sapiens.GRCh38.dna.chromosome.1.fa    ##index reference genome
 bwa mem Homo_sapiens.GRCh38.dna.chromosome.1.fa miniMNM00065.ch1.fasta | samtools view -Sb - > miniMNM00065aln-ch1.bam   ###alignreads to chromsome1 
+
